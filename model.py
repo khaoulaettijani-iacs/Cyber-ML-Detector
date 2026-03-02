52bfea7a537caac3baeb9f76a99fdd68ab1aa37f
Ettijani Khaoula <khaoulaettijani19@gmail.com>
2026-03-02 01:00:32 +0100

    Prepare data for training

--- model.py
+++ model.py
@@ -38,3 +38,9 @@ def generate_data(n_samples):
     return pd.DataFrame(data)
 df = generate_data(N)
 print(df.head(10))
+
+# Prepare data for modeling (SPLIT TRAIN / TEST)
+X = df[["login_attempts", "failed_attempts", "failed_ratio",
+        "avg_time_between", "ip_change_frequency"]]
+y = df["label"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
